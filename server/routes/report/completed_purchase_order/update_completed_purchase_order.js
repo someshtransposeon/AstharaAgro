d9b9a3141563bb910d75b728b9bc7bfea9e31091
luckykumarirai <luckykumari112000@gmail.com>
2022-02-04 19:02:14 +0530

    update

--- server/routes/report/completed_purchase_order/update_completed_purchase_order.js
+++ server/routes/report/completed_purchase_order/update_completed_purchase_order.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Purchase_order = require('../../../models/report/completed_purchase_order/completed_purchase_order');
-
-router.put('/update_flag_completed_purchase_order/:id', (req, res)=>{
-    var Purchase = Purchase_order({
+router.put('/update_flag_completed_purchase_order/:id',(req, res) =>{
+    var update = {
         flag:req.body.flag,
-    })
-    Purchase_order.findOneAndUpdate({'_id':req.params.id}, Purchase)
-    .then((purchase_order) => {
-        if(purchase_order){
-            var message = {message: "all completed Purchase Order sucessfully updated"};
+    }
+    Purchase_order.findOneAndUpdate({'_id':req.params.id},update)
+    .then((user) => {
+        if(user){
+            var message = { message: "user sucessfully updated" };
             res.json(message);
         }else{
-            var message = { message: "Record not found" };
+            var message = { message: "user not found" };
             res.json(message);
         }
     }).catch(err => {
         console.log(err);
-        var message = { message: "Somthing Went Wrong!", success: false, err: err };
+        var message = {message:"Something went wrong!", success: false, err: err };
         res.json(message);
     })
 });
-
 module.exports = router;
