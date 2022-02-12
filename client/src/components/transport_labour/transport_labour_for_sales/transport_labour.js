4f2a5ea538f2e74fba49fe61778ae2c44261b3ad
Pankaj Kumar Bijarniya <pankajbijarniya2004@gmail.com>
2022-02-12 13:56:54 +0530

    updated

--- client/src/components/transport_labour/transport_labour_for_sales/transport_labour.js
+++ client/src/components/transport_labour/transport_labour_for_sales/transport_labour.js
@@ -1,4 +1,4 @@
-import { faCamera, faMinusCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React, {useState, useEffect} from 'react';
 import { View, StyleSheet,Platform, ScrollView, SafeAreaView, Text, Alert} from 'react-native';
@@ -7,7 +7,6 @@ import { useHistory } from 'react-router-dom';
 import swal from '@sweetalert/with-react';
 import { userId } from '../../../utils/user';
 import { all_completed_purchase_orders } from '../../../services/pickup_api';
-import Scanner from '../../barcode/scanner';
 import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
 const theme = {
@@ -35,7 +34,9 @@ export default function AddTransportLabour(props,{ navigation }) {
     const [acpo, setACPO] = useState();
     const [visible3, setVisible3] = useState(false);
     const [ data, setData ] = useState('Not Found');
-    const [msg, setMsg] = useState(2);
+    const [flag, setFlag] = useState(false);
+    const [msg, setMsg] = useState("");
+    const [addedItems, setAddedItems] = useState([]);
 
     let history = useHistory();
 
@@ -46,41 +47,57 @@ export default function AddTransportLabour(props,{ navigation }) {
             setACPO(result);
         })
 
-    })
+    },[acpo])
 
-    const ItemChange = (index, item) => {
+    const ItemChange = () => {
 
         var val=acpo.find(o => o.barcode === data);
         if(val==undefined){
-            setMsg(1);
+            setMsg("Error!! Item not found");
         }
         else{
-            setMsg(3);
-            const values = [...items];
-            values.push({orderId: val.purchase_order.orderId, itemName: val.purchase_order.items.itemName, Grade: val.purchase_order.items.Grade, quantity: val.purchase_order.items.quantity});
-            setItems(values);
-            fetch(`http://localhost:5000/update_flag_completed_purchase_order/${val._id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    flag:1,
-                    vehicle_number: vNumber,
-                    driver_name: driverName,
-                    driver_mobile_no: driverMobileNumber,
-                    labour_name: labourName,
-                    labour_mobile_no: labourMobileNumber,
-                })
-            }).then(res => res.json())
-            .catch(error => console.log(error))
-            .then(data => {
-                // alert(data.message);
-                // console.log(data);
-            });
+            if(addedItems.includes(data) || val.flag==1){
+                setMsg("Error!! Item is already scanned");
+            }
+            else{
+                const values1 = [...addedItems];
+                values1.push(data);
+                setAddedItems(values1); 
+
+                var beepsound = new Audio('./beep-02.mp3');   
+                beepsound.play();
+
+                const values = [...items];
+                values.push({orderId: val.purchase_order.orderId, itemName: val.purchase_order.items.itemName, Grade: val.purchase_order.items.Grade, quantity: val.purchase_order.items.quantity});
+                setItems(values);
+                fetch(`http://localhost:5000/update_flag_completed_purchase_order/${val._id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        flag:1,
+                        vehicle_number: vNumber,
+                        driver_name: driverName,
+                        driver_mobile_no: driverMobileNumber,
+                        labour_name: labourName,
+                        labour_mobile_no: labourMobileNumber,
+                    })
+                }).then(res => res.json())
+                .catch(error => console.log(error))
+                .then(data => {
+                    // alert(data.message);
+                    // console.log(data);
+                });
+                setMsg("Success!! Added");
+            }
         }
-        setData('Not Found');
+        setData("Not Found");
     };
+
+    if(flag==false && data!="Not Found"){
+        ItemChange();
+    }
 
     function submitForm() {
         fetch(`http://localhost:5000/create_transport_labour_for_sales`, {
@@ -145,8 +162,6 @@ export default function AddTransportLabour(props,{ navigation }) {
         showModal();
     }
 
-    const containerStyle = {backgroundColor: 'white',width: '35%', alignSelf: 'center'};
-
     return (
         <Provider theme={theme}>
         <SafeAreaView>
@@ -171,48 +186,43 @@ export default function AddTransportLabour(props,{ navigation }) {
                     <TextInput style={styles.input} mode="outlined" label="Labour Name" value={labourName} onChangeText={labourName => setLabourName(labourName)} />
                     <TextInput style={styles.input} mode="outlined" label="Labour Mobile Number" value={labourMobileNumber} onChangeText={labourMobileNumber => setLabourMobileNumber(labourMobileNumber)} />
                     <TextInput style={styles.input} mode="outlined" label="Charge" value={charge} onChangeText={charge => setCharge(charge)} />
-                    <Button mode="contained" style={styles.button} onPress={() => scan()} icon={() => <FontAwesomeIcon icon={ faCamera } />}>Start Scan</Button>
-                    {/* <Modal visible={visible3} onDismiss={hideModal} contentContainerStyle={containerStyle}> */}
-                    { visible3 &&
-                    <>
-                        <BarcodeScannerComponent
-                            width="100%"
-                            height="100%"
-                            onUpdate={(err, result) => {
-                                if (result) setData(result.text)
-                            }}
-                        />
-                        <View style={{padding: '5px'}}>
-                            <TextInput style={styles.input} mode="outlined" label="Data" value={data} onChangeText={data => setData(data)} />
-                            {msg && msg==3 &&
-                                <Text style={{color:"green"}}>Successfully Added!!</Text>
-                            }
-                            {msg && msg==1 &&
-                                <Text style={{color:"red"}}>Order Not Found</Text>
-                            }
-                            {data && data!="Not Found" &&
-                                <>
-                                    <Button onPress={()=>ItemChange()}>Add</Button>
-                                </>
-                            }
-                        </View>
-                    </>
-                    }
-                {/* </Modal> */}
                     {items.map((it, index) => (
                         <View>
                             <Menu
                             visible={visible[index]}
                             onDismiss={()=>closeMenu(index)}
-                            anchor={<Button style={styles.input} mode="outlined"  onPress={()=>openMenu(index)}>{it.orderId} </Button>}>
+                            anchor={<Button style={styles.input} mode="outlined"  onPress={()=>openMenu(index)}>{it.itemName+" "+it.Grade+" "+it.quantity}</Button>}>
                             </Menu>
                         </View>
                     ))}
-
+                    { visible3 &&
+                    <>
+                        <BarcodeScannerComponent
+                            width="50%"
+                            onUpdate={(err, result) => {
+                                if (result) setData(result.text)
+                            }}
+                        />
+                        <Text>{msg}</Text>
+                        {!flag ?
+                            <Button onPress={()=>setFlag(true)}>Manually Add</Button>
+                            :
+                            <Button onPress={()=>setFlag(false)}>Start Scan</Button>
+                        }
+                        {flag &&
+                            <View style={{padding: '5px', display: 'flex'}}>
+                                <TextInput style={styles.input} mode="outlined" label="Data" value={data} onChangeText={data => setData(data)} />
+                                <Button onPress={()=>ItemChange()}>Add</Button>
+                            </View>
+                        }
+                    </>
+                    }
+                    {!visible3 &&
+                        <Button mode="contained" style={styles.button} onPress={() => scan()} icon={() => <FontAwesomeIcon icon={ faCamera } />}>Start Scan</Button>
+                    }
                     <Button mode="contained" style={styles.button} onPress={()=>submitForm()}>Submit</Button>
                     </Card.Content>
                 </Card>
-                
             </View>
         </ScrollView>
         </SafeAreaView>
