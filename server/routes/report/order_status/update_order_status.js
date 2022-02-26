5c2cb6609d6bf3d2059bcd2fcce4e3fb1d30bd26
luckykumarirai <luckykumari112000@gmail.com>
2022-02-26 20:59:18 +0530

    upadate

--- server/routes/report/order_status/update_order_status.js
+++ server/routes/report/order_status/update_order_status.js
@@ -42,7 +42,7 @@ router.put('/update_order_spit_status/:id',(req, res) =>{
     })
 });
 
-router.put('/update_order_quantity/:id',(req, res) =>{
+router.put('/update_order_item_quantity/:id',(req, res) =>{
     var update = {
         quantity: req.body.quantity,
     }
@@ -62,9 +62,48 @@ router.put('/update_order_quantity/:id',(req, res) =>{
     })
 });
 
-router.put('/update_order_quantity/:orderid/:item_name/:item_grade',(req, res) =>{
+router.put('/update_order_quantity/:orderid/:item_name/:item_grade/:quantity',(req, res) =>{
     var update = {
         quantity: req.body.quantity,
+    }
+    order_status.findOneAndUpdate({'orderId':req.params.orderid,'item_name':req.params.item_name,'item_garde':req.params.item_grade,'quantity':req.params.quantity},update)
+    .then((order) => {
+        if(order){
+            var message = { message: "order status quantity updated sucessfully" };
+            res.json(message);
+        }else{
+            var message = { message: "order not found" };
+            res.json(message);
+        }
+    }).catch(err => {
+        console.log(err);
+        var message = {message:"Something went wrong!", success: false, err: err };
+        res.json(message);
+    })
+});
+
+router.put('/update_order_item_status/:orderid/:item_name/:item_grade',(req, res) =>{
+    var update = {
+        status: req.body.status,
+    }
+    order_status.findOneAndUpdate({'orderId':req.params.orderid,'item_name':req.params.item_name,'item_garde':req.params.item_grade},update)
+    .then((order) => {
+        if(order){
+            var message = { message: "order status quantity updated sucessfully" };
+            res.json(message);
+        }else{
+            var message = { message: "order not found" };
+            res.json(message);
+        }
+    }).catch(err => {
+        console.log(err);
+        var message = {message:"Something went wrong!", success: false, err: err };
+        res.json(message);
+    })
+});
+router.put('/update_order_item_split_status/:orderid/:item_name/:item_grade',(req, res) =>{
+    var update = {
+        split_status: req.body.split_status,
     }
     order_status.findOneAndUpdate({'orderId':req.params.orderid,'item_name':req.params.item_name,'item_garde':req.params.item_grade},update)
     .then((order) => {
